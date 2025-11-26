c864e7cf5e084d50ea09129a217a51bad379b689
dikie001 <omondidickens255@gmail.com>
2025-11-27 02:04:52 +0530

    `Added react-router-dom and related dependencies` feat:added onboarding and implemented routing

---
+++ src/routes/index.route.tsx
@@ -0,0 +1,15 @@
+
+  import { Onboarding } from '@/features/onboarding/Onboarding'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/onboarding' element={<Onboarding/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
